85463bd21d237fdf77ec7b095048aa7a0f4590dd
GouravMukherjee <gouravmukherjee09@gmail.com>
2019-05-02 08:23:36 +0530

    added mobile views

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import ReactAudioPlayer from 'react-audio-player';
 import './App.css';
-import one from './assets/1.png';
-import two from './assets/2.png';
-import three from './assets/3.png';
-import four from './assets/4.png';
+// import one from './assets/1.png';
+// import two from './assets/2.png';
+// import three from './assets/3.png';
+// import four from './assets/4.png';
 import five from './assets/5.png';
 import five1 from './assets/51.png';
 import five2 from './assets/52.png';
@@ -13,74 +13,113 @@ import six from './assets/6.png';
 import six1 from './assets/61.png';
 import six2 from './assets/62.png';
 import music from './assets/music.png';
-var once=true;
+
+import background from './assets/home/Courtside_desktop.png'
+import backgroundMobile from './assets/home/Courtside_mobile.png'
+
+var once = true;
 class App extends Component {
 
-  componentDidMount(){
+  componentDidMount() {
     var list = document.getElementsByTagName("audio")[0]
     list.classList.add("mystyle")
     // console.log("audios tag",list)
-    document.getElementsByClassName('react-audio-player ')[0].loop=true
+    document.getElementsByClassName('react-audio-player ')[0].loop = true
   }
 
-  soundmute=()=>{
-    if(once){
-      document.getElementsByClassName('react-audio-player ')[0].muted=true;
-      once=false;
+  soundmute = () => {
+    if (once) {
+      document.getElementsByClassName('react-audio-player ')[0].muted = true;
+      once = false;
     }
-    else{
-      document.getElementsByClassName('react-audio-player ')[0].muted=false
-      once=true;
+    else {
+      document.getElementsByClassName('react-audio-player ')[0].muted = false
+      once = true;
     }
   }
 
-  click1=(e)=>{
+  click1 = (e) => {
     console.log(e.target);
     // e.target.style.display ="none";
-    document.getElementById("five").style.display="none"
-    document.getElementById("five1").style.display="inline"
-    document.getElementById("six").style.display="inline"
+    document.getElementById("five").style.display = "none"
+    document.getElementById("five1").style.display = "inline"
+    document.getElementById("six").style.display = "inline"
 
   }
 
-  click2=(e)=>{
+  click2 = (e) => {
     console.log(e.target);
     // e.target.style.display ="none";
-    document.getElementById("six").style.display="none"
-    document.getElementById("six1").style.display="inline"
+    document.getElementById("six").style.display = "none"
+    document.getElementById("six1").style.display = "inline"
   }
 
-  click3=(e)=>{
+  click3 = (e) => {
     console.log(e.target);
     // e.target.style.display ="none";
-    document.getElementById("six1").style.display="none";
-    document.getElementById("five1").style.display="none";
-    document.getElementById("six2").style.display="inline";
-    document.getElementById("five2").style.display="inline";
+    document.getElementById("six1").style.display = "none";
+    document.getElementById("five1").style.display = "none";
+    document.getElementById("six2").style.display = "inline";
+    document.getElementById("five2").style.display = "inline";
   }
 
-  click4=(e)=>{
-    document.getElementById("six2").style.display="none";
-    document.getElementById("five2").style.display="none";
-    document.getElementById("five3").style.display="inline";
+  click4 = (e) => {
+    document.getElementById("six2").style.display = "none";
+    document.getElementById("five2").style.display = "none";
+    document.getElementById("five3").style.display = "inline";
   }
 
-  click5=(e)=>{
+  click5 = (e) => {
     let path = `/store`;
-      this.props.history.push("/store")
+    this.props.history.push("/store")
   }
 
   render() {
     return (
       <div className="App">
-      <ReactAudioPlayer
-        src='http://goldfirestudios.com/proj/howlerjs/sound.ogg'
-        autoPlay
-        controls
-        ref={(element) => { this.rap = element; }}
-      />
-    
-      <div >
+        <ReactAudioPlayer
+          src='http://goldfirestudios.com/proj/howlerjs/sound.ogg'
+          autoPlay
+          controls
+          ref={(element) => { this.rap = element; }}
+        />
+        <div className="blue"></div>
+        <div className="container">
+          <div>
+            <img
+              className="logo"
+              src={window.screen.width > 560 ? background : backgroundMobile} />
+          </div>
+          <div>
+            <img src={five} className="images five" id="five" onClick={this.click1} />
+          </div>
+          <div>
+            <img src={five1} className="images five1" id="five1" onClick={this.click2} />
+          </div>
+          <div>
+            <img src={six} className="images six" id="six" onClick={this.click2} />
+          </div>
+          <div>
+            <img src={six1} className="images six1" id="six1" onClick={this.click3} />
+          </div>
+          <div>
+            <img src={five2} className="images five2" id="five2" onClick={this.click4} />
+          </div>
+          <div>
+            <img src={six2} className="images six2" id="six2" onClick={this.click4} />
+          </div>
+          <div>
+            <a href="/store">
+              <img src={five3} className="images five3" id="five3" />
+            </a>
+          </div>
+          <div>
+            <img src={music} className="music " id="music" onClick={this.soundmute} />
+          </div>
+          <div><img src="/static/media/portal.63d0b8bd.png" class="portal" data-toggle="modal" data-target="#myModal" id="portal" /></div>
+        </div>
+
+        {/* <div >
       <img src={one} className="images one" />
       </div>
       <div>
@@ -117,8 +156,9 @@ class App extends Component {
       </div>
       <div>
       <img src={music} className="music " id="music" onClick={this.soundmute}/>
-      </div>
-      <div className="blue"></div>
+      </div>  */}
+
+
       </div>
     );
   }
--- src/store.js
+++ src/store.js
@@ -33,207 +33,199 @@ import paintings from './assets/store/portal/paintings.png';
 import sculpture from './assets/store/portal/sculpture.png';
 import ReactAudioPlayer from 'react-audio-player';
 
+import background from './assets/store/final/Newworld_desktop.png'
+import backgroundMobile from './assets/store/final/Newworld_mobile.png'
+
 class Store extends Component {
 
-  componentDidMount(){
+  componentDidMount() {
     var list = document.getElementsByTagName("audio")[0]
     list.classList.add("mystyle")
     // console.log("audios tag",list)
-    document.getElementsByClassName('react-audio-player ')[0].loop=true
+    document.getElementsByClassName('react-audio-player ')[0].loop = true
   }
 
-  click1=()=>{
-    document.getElementById("store10l").style.display="none"
-    document.getElementById("store11").style.display="inline"
+  click1 = () => {
+    document.getElementById("store10l").style.display = "none"
+    document.getElementById("store11").style.display = "inline"
   }
 
-  click2=()=>{
-    document.getElementById("store11").style.display="none"
-    document.getElementById("store12").style.display="inline"
+  click2 = () => {
+    document.getElementById("store11").style.display = "none"
+    document.getElementById("store12").style.display = "inline"
   }
 
-  click3=()=>{
-    document.getElementById("store12").style.display="none"
-    document.getElementById("store13").style.display="inline"
+  click3 = () => {
+    document.getElementById("store12").style.display = "none"
+    document.getElementById("store13").style.display = "inline"
   }
 
-  click4=()=>{
+  click4 = () => {
 
   }
 
-  tuhutz=()=>{
-    document.getElementById("myModal").style.display="none"
+  tuhutz = () => {
+    document.getElementById("myModal").style.display = "none"
   }
 
-  contribute=()=>{
-    document.getElementById("myModal").style.display="none"
+  contribute = () => {
+    document.getElementById("myModal").style.display = "none"
   }
 
   render() {
     return (
       <div className="App1">
-      <ReactAudioPlayer
-        src='http://goldfirestudios.com/proj/howlerjs/sound.ogg'
-        autoPlay
-        controls
-        ref={(element) => { this.rap = element; }}
-      />
-        <div>
-        <img src={store1} className="images store1" />
-        </div>
-        <div>
-        <img src={store2} className="images store2" />
-        </div>
-        <div>
-        <img src={store3} className="images store3" />
-        </div>
-        <div>
-        <img src={store4} className="images store4" />
-        </div>
-        <div>
-        <img src={store5l} className="images store5l" />
-        </div>
-        <div>
-        <img src={store5r} className="images store5r" />
-        </div>
-        <div>
-        <a href="/burger">
-        <img src={store6l} className="images store6l" /></a>
-        </div>
-        <div>
-        <a href="http://www.tuhutzs.com/#/vending-machine">
-        <img src={store6m} className="images store6m" /></a>
-        </div>
-        <div>
-        <a href="/kiosk">
-        <img src={store6r} className="images store6r" /></a>
-        </div>
-        <div>
-        <img src={store7l} className="images store7l" />
-        </div>
-        <div>
-        <img src={store7r} className="images store7r" />
-        </div>
-        <div>
-        <img src={store8l} className="images store8l" />
-        </div>
-        <div>
-        <img src={store8r} className="images store8r" />
-        </div>
-        <div>
-        <img src={store9} className="images store9" />
-        </div>
-        <div>
-        <img src={store101l} className="images store101l" />
-        </div>
-        <div>
-        <img src={store101m} className="images store101m" />
-        </div>
-        <div>
-        <img src={store10l} className="images store10l" id="store10l" onClick={this.click1} />
-        </div>
-        <div>
-        <img src={store10r} className="images store10r" />
-        </div>
-        <div>
-        <img src={store11} className="images store11"  id="store11" onClick={this.click2} />
-        </div>
-        <div>
-        <img src={store12} className="images store12" id="store12" onClick={this.click3} />
-        </div>
-        <div>
-        <img src={portal} className="portal" data-toggle="modal" data-target="#myModal" id="portal"  />
-        </div>
-        <div>
-          
-        <img src={store13} className="images store13" id="store13"  />
-        </div>
+        <ReactAudioPlayer
+          src='http://goldfirestudios.com/proj/howlerjs/sound.ogg'
+          autoPlay
+          controls
+          ref={(element) => { this.rap = element; }}
+        />
         <div className="black"></div>
-        <div id="myModal" class="modal fade" role="dialog">
-        
-        <div className="modal-dialog modal-md">
-            <div className="modal-content">
-                <div className="modal-body">
-                <img src={tuhutz} className="tuhutz"  id="tuhutz" data-toggle="modal" 
-                data-dismiss="modal" data-target="#tuhutzss" onClick={this.tuhutz} />
-                <img src={sculpture} className="tuhutz"  id="order2" />
-                <img src={paintings} className="tuhutz"  id="order1" />
-                <img src={hatzs} className="tuhutz"  id="order1" />
-                <img src={Films} className="tuhutz"  id="order1" />
-                <img src={contribute} className="tuhutz" data-toggle="modal" 
-                data-dismiss="modal" data-target="#contribute" id="order1" onClick={this.contribute} />
-                </div>
-            </div>
+
+        <div className="container-fluid">
+          <div>
+            <img 
+              className="storeBackground" 
+              src={window.screen.width > 560 ? background : backgroundMobile} />
+          </div>
+       
+          <div>
+            <a href="/burger">
+              <img src={store6l} className="images store6l" /></a>
+          </div>
+          <div>
+            <a href="http://www.tuhutzs.com/#/vending-machine">
+              <img src={store6m} className="images store6m" /></a>
+          </div>
+          <div>
+            <a href="/kiosk">
+              <img src={store6r} className="images store6r" /></a>
+          </div>
+          <div>
+            <img src={store7l} className="images store7l" />
+          </div>
+          <div>
+            <img src={store7r} className="images store7r" />
+          </div>
+          <div>
+            <img src={store8l} className="images store8l" />
+          </div>
+          <div>
+            <img src={store8r} className="images store8r" />
+          </div>
+          <div>
+            <img src={store101l} className="images store101l" />
+          </div>
+          <div>
+            <img src={store101m} className="images store101m" />
+          </div>
+          <div>
+            <img src={store10l} className="images store10l" id="store10l" onClick={this.click1} />
+          </div>
+          <div>
+            <img src={store10r} className="images store10r" />
+          </div>
+          <div>
+            <img src={store11} className="images store11" id="store11" onClick={this.click2} />
+          </div>
+          <div>
+            <img src={store12} className="images store12" id="store12" onClick={this.click3} />
+          </div>
+          <div>
+            <img src={portal} className="portal" data-toggle="modal" data-target="#myModal" id="portal" />
+          </div>
+          <div>
+
+            <img src={store13} className="images store13" id="store13" />
+          </div>
         </div>
+        {/* <div className="black"></div> */}
+        <div id="myModal" class="modal fade" role="dialog">
+
+          <div className="modal-dialog modal-md">
+            <div className="modal-content">
+              <div className="modal-body">
+                <img src={tuhutz} className="tuhutz" id="tuhutz" data-toggle="modal"
+                  data-dismiss="modal" data-target="#tuhutzss" onClick={this.tuhutz} />
+                <img src={sculpture} className="tuhutz" id="order2" />
+                <img src={paintings} className="tuhutz" id="order1" />
+                <img src={hatzs} className="tuhutz" id="order1" />
+                <img src={Films} className="tuhutz" id="order1" />
+                <img src={contribute} className="tuhutz" data-toggle="modal"
+                  data-dismiss="modal" data-target="#contribute" id="order1" onClick={this.contribute} />
+              </div>
+            </div>
+          </div>
         </div>
 
 
         <div id="tuhutzss" class="modal fade " role="dialog">
-        
-        <div className="modal-dialog modal-md">
+
+          <div className="modal-dialog modal-md">
             <div className="modal-content">
-                <div className="modal-body karl-bio">
+              <div className="modal-body karl-bio">
                 <div className="col-xs-8">
-                Tuhutzs is the artistic name for Karl Bauer.
-                Born England, United Kingdom 04/07/1988
-                Specialising in video production Karl adopts many disciplines to describe & present his work.
-                Contact: karl.bauer@ymail.com
-                Artist Statement
-                It is practically impossible not to produce some form of waste multiple times per day from
-                something you interact with or consume. The bins that stray our planet in all possible places, in a
-                variety of style and colours lay open a view displaying a relationship with our planet. As our
-                civilisation opens its doors to the information age. I feel the food we buy is killing us, the water is
-                bottled in plastic. The ultimate need to consume is laid bare. Import, export, the seas run full of
-                cargo ships. As we flood our planet with products and packaging we blame “you”. The notion “be
-                brave” represents a positive message in the physical and mental struggle that we face today.
-                A bin has never been as cool as it is now.
-                As the Chinese develop new ambitious infrastructure throughout South America, opening new
-                shipping canals and trade deals as the BRF Belt and Road Forum, we can see a new carving of
-                globalisation reaching further out changing the worlds shape. As products, choices and options
-                begin to fill the shelves of developing nations, the unconscious need for bins, their stability and
-                functionality, will have to match the phase of an advancing individual demand for more.
-                We are not to blame for having been flooded with excess packaging, one use items, pre packed,
-                processed living. In the western world our choices exceed that of developing worlds. We are
-                victims of design, choice, private profiteering and unstable global management. We all require
-                basic food and water, but do we really need the thousands of options and designs, left solely
-                upon an end choice that is, what can you afford to buy.
+                  Tuhutzs is the artistic name for Karl Bauer.
+                  Born England, United Kingdom 04/07/1988
+                  Specialising in video production Karl adopts many disciplines to describe & present his work.
+                  Contact: karl.bauer@ymail.com
+                  Artist Statement
+                  It is practically impossible not to produce some form of waste multiple times per day from
+                  something you interact with or consume. The bins that stray our planet in all possible places, in a
+                  variety of style and colours lay open a view displaying a relationship with our planet. As our
+                  civilisation opens its doors to the information age. I feel the food we buy is killing us, the water is
+                  bottled in plastic. The ultimate need to consume is laid bare. Import, export, the seas run full of
+                  cargo ships. As we flood our planet with products and packaging we blame “you”. The notion “be
+                  brave” represents a positive message in the physical and mental struggle that we face today.
+                  A bin has never been as cool as it is now.
+                  As the Chinese develop new ambitious infrastructure throughout South America, opening new
+                  shipping canals and trade deals as the BRF Belt and Road Forum, we can see a new carving of
+                  globalisation reaching further out changing the worlds shape. As products, choices and options
+                  begin to fill the shelves of developing nations, the unconscious need for bins, their stability and
+                  functionality, will have to match the phase of an advancing individual demand for more.
+                  We are not to blame for having been flooded with excess packaging, one use items, pre packed,
+                  processed living. In the western world our choices exceed that of developing worlds. We are
+                  victims of design, choice, private profiteering and unstable global management. We all require
+                  basic food and water, but do we really need the thousands of options and designs, left solely
+                  upon an end choice that is, what can you afford to buy.
                 </div>
                 <div className="col-xs-4">
-                <img src={Karl} className="karl"  id="karl" />
+                  <img src={Karl} className="karl" id="karl" />
                 </div>
-                </div>
+              </div>
             </div>
-        </div>
+          </div>
         </div>
 
         <div id="contribute" class="modal fade" role="dialog">
-        
-        <div className="modal-dialog modal-md">
+
+          <div className="modal-dialog modal-md">
             <div className="modal-content">
-                <div className="modal-body">
+              <div className="modal-body">
                 <div className="col-xs-12">
-                <h3>Web Developer</h3>
-                <p>Amanjeet Singh - India - amanjeetgilard@yahoo.in</p>
-                <p>Darren Murphy - United Kingdom</p>
-               <h3> Character designers</h3>
-               <p> Luca Tomas Prino - Argentina - lucaprino@yahoo.com</p>
-                <p>Clarita S. Moyano - Argentina - claritasmoyano@gmail.com</p>
-               <h3> Film & Photography</h3>
-               <p> Curtis Rowlands - United Kingdom - info@curtisrowland.com</p>
-               <p>Denis Mcmahon - United Kingdom - Denis.mcm@hotmail.com</p> 
-               <p> Artists & Curators</p>
-               <p> Bobby Benjamin - United Kingdom - benjamithefuture@hotmail.co.uk</p>
-               <h3> Music Prodcuers</h3>
-               <p> Shxxr - Adam Trattles - United Kingdom - shxxrofficial@gmail.com</p>
+                  <h3>Web Developer</h3>
+                  <p>Amanjeet Singh - India - amanjeetgilard@yahoo.in</p>
+                  <p>Darren Murphy - United Kingdom</p>
+                  <h3> Character designers</h3>
+                  <p> Luca Tomas Prino - Argentina - lucaprino@yahoo.com</p>
+                  <p>Clarita S. Moyano - Argentina - claritasmoyano@gmail.com</p>
+                  <h3> Film & Photography</h3>
+                  <p> Curtis Rowlands - United Kingdom - info@curtisrowland.com</p>
+                  <p>Denis Mcmahon - United Kingdom - Denis.mcm@hotmail.com</p>
+                  <p> Artists & Curators</p>
+                  <p> Bobby Benjamin - United Kingdom - benjamithefuture@hotmail.co.uk</p>
+                  <h3> Music Prodcuers</h3>
+                  <p> Shxxr - Adam Trattles - United Kingdom - shxxrofficial@gmail.com</p>
                 </div>
                 <br />
-                <img src={upparrows} style={{textAlign:"center", width:"20%"}} />
-                </div>
+                <img src={upparrows} style={{ textAlign: "center", width: "20%" }} />
+              </div>
             </div>
-        </div>
+          </div>
         </div>
 
-        
+
       </div>
     );
   }
