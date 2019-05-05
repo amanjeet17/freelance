0792f06ae95fd6aaf47419074e69fa3114efb6a3
GouravMukherjee <gouravmukherjee09@gmail.com>
2019-05-05 18:34:59 +0530

    removed unnecessary imports

--- src/basura.js
+++ src/basura.js
@@ -2,20 +2,13 @@ import React, { Component } from 'react';
 
 import './basura.css';
 
-import background from './assets/basura/desktop.png'
-import backgroundMobile from './assets/basura/mobile.png'
-
 class Basura extends Component {
     render() {
         return (
             <React.Fragment>
                 <div className="black"></div>
                 <div className="App1"></div>
-                <div className="image">
-                    {/* <img
-                        className="basuraImage"
-                        src={window.screen.width > 560 ? background : backgroundMobile} /> */}
-                </div>
+                <div className="image"></div>
             </React.Fragment>
         )
     }
